ae5ca95572ee71c98030352428dce54fdc054c6c
LevBaranov <lev.l.baranov@gmail.com>
2023-03-17 20:00:20 +0500

    Сделал повторный запрос, если сервис отправляет 500

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import requests
+from time import sleep
 
 # Enabling logging
 logging.basicConfig(level=logging.INFO,
@@ -14,15 +15,20 @@ class Converter:
         self.url_converter = os.getenv("CONVERTER_URL")
         self.auth_token = os.getenv("CONVERTER_TOKEN")
 
-    def audio_to_text(self, file_name: str) -> str:
+    def audio_to_text(self, file_name: str) -> str | None:
         url = f"{self.url_converter}/transcription"
 
         headers = {"Authorization": f"Bearer {self.auth_token}"}
         r = requests.post(url, headers=headers, files={'file': open(file_name, 'rb')})
         if r.status_code == 200:
             resp_json = r.json()
-            response = resp_json["response_text"]
+            logger.info(f"Response: {r.status_code} {r.text}")
+            return resp_json["response_text"]
+        elif r.status_code == 500:
+            logger.info(f"Response: {r.status_code} {r.text}")
+            sleep(2)
+            self.audio_to_text(file_name)
         else:
-            response = None
-        logger.info(f"Response: {r.status_code} {r.text}")
-        return response
+            return None
+
+
